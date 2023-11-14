904b1763c577790ea8b9c67a99e5d79ad9b9e569
ahmaddzidnii <zdngaming312@gmail.com>
2023-11-14 21:51:56 +0700

    1:30:00

---
+++ app/(platform)/(clerk)/layout.tsx
@@ -0,0 +1,5 @@
+const ClerkLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="flex items-center justify-center min-h-screen">{children}</div>;
+};
+
+export default ClerkLayout;
