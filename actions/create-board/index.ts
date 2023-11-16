3187a73292dcc7d809a38523fa5814388a03782c
ahmaddzidnii <zdngaming312@gmail.com>
2023-11-16 17:54:06 +0700

    4:16:00

---
+++ actions/create-board/index.ts
@@ -0,0 +1,37 @@
+"use server";
+
+import { auth } from "@clerk/nextjs";
+import { InputType, ReturnType } from "./types";
+import { revalidatePath } from "next/cache";
+import { createSafeAction } from "@/lib/create-safe-action";
+import { CreateBoard } from "./schema";
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId } = auth();
+  if (!userId) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+
+  const { title } = data;
+
+  let board;
+
+  try {
+    board = await prisma?.board.create({
+      data: {
+        title,
+      },
+    });
+  } catch (error) {
+    return {
+      error: "Failed to create board",
+    };
+  }
+
+  revalidatePath(`board/${board?.id}`);
+  return { data: board };
+};
+
+export const createBoard = createSafeAction(CreateBoard, handler);
--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,17 +1,28 @@
 "use client";
-import { create } from "@/actions/create-board";
-import { useFormState } from "react-dom";
 import { FormInput } from "./form-input";
 import { FormButton } from "./form-button";
+import { createBoard } from "@/actions/create-board";
+import { useAction } from "@/hooks/use-action";
 export const Form = () => {
-  const initiaState = { message: null, errors: {} };
-  const [state, dispatch] = useFormState<Promise<any>>(create as any, initiaState);
+  const { execute, fieldErrors } = useAction(createBoard, {
+    onSuccess: (data) => {
+      console.log(data);
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+    execute({ title });
+  };
   return (
-    <form action={dispatch}>
+    <form action={onSubmit}>
       <div className="flex flex-col space-y-2">
-        <FormInput errors={state?.errors} />
+        <FormInput errors={fieldErrors as any} />
       </div>
-     <FormButton/>
+      <FormButton />
     </form>
   );
 };
